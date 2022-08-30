7bad42beef94f78f72e6d2b58aa85dd200c53c85
ShawnLiu <lfq1471705517@163.com>
2022-08-30 15:45:37 +0800

    tar hand-made rootfs

--- other/debug_related/hand_made_rootfs/rootfs/lib/gcc/arm-mini2440-linux-gnueabi/6.5.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "./configure --prefix=/home/liu/src/buildroot-2022.02.4/output/host --sysconfdir=/home/liu/src/buildroot-2022.02.4/output/host/etc --enable-static --target=arm-mini2440-linux-gnueabi --with-sysroot=/home/liu/src/buildroot-2022.02.4/output/host/arm-mini2440-linux-gnueabi/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/home/liu/src/buildroot-2022.02.4/output/host --with-mpc=/home/liu/src/buildroot-2022.02.4/output/host --with-mpfr=/home/liu/src/buildroot-2022.02.4/output/host --with-pkgversion='Buildroot 2022.02.4' --with-bugurl=http://bugs.buildroot.net/ --without-zstd --disable-libquadmath --disable-libquadmath-support --enable-tls --enable-threads --without-isl --without-cloog --with-float=soft --with-abi=aapcs-linux --with-cpu=arm920t --with-float=soft --with-mode=arm --enable-languages=c --with-build-time-tools=/home/liu/src/buildroot-2022.02.4/output/host/arm-mini2440-linux-gnueabi/bin --enable-shared --disable-libgomp";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "arm920t" }, { "float", "soft" }, { "mode", "arm" }, { "tls", "gnu" } };
